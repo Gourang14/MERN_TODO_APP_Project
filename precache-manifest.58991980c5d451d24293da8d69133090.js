e5b370941c6129344ec3d273fb8b6b2b75dd7b9a
Gourang14 <gourangagarwal1104@gmail.com>
2025-04-21 23:42:37 +0530

    Updates

---
+++ precache-manifest.58991980c5d451d24293da8d69133090.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "666e86dc69015e851eb7897f81c44dee",
+    "url": "/MERN_TODO_APP_Project/index.html"
+  },
+  {
+    "revision": "bf43c33503a0a31db110",
+    "url": "/MERN_TODO_APP_Project/static/css/main.823a9ef2.chunk.css"
+  },
+  {
+    "revision": "cb38f3bd2c17ae7b71f4",
+    "url": "/MERN_TODO_APP_Project/static/js/2.4a122d4d.chunk.js"
+  },
+  {
+    "revision": "41d8536d0e796ab7f5f189dacfab01da",
+    "url": "/MERN_TODO_APP_Project/static/js/2.4a122d4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf43c33503a0a31db110",
+    "url": "/MERN_TODO_APP_Project/static/js/main.66f4358b.chunk.js"
+  },
+  {
+    "revision": "04a2968a84e7089ef713",
+    "url": "/MERN_TODO_APP_Project/static/js/runtime-main.b932c300.js"
+  }
+]);
